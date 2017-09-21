890d8368247f1cae503ce34088082af32b216346
Ben Fradet <benjamin.fradet@gmail.com>
2017-09-21 14:57:50 +0100

    Add CI/CD to deploy all images into our Bintray (closes #16)

---
+++ .travis/deploy.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+project=$1
+tag=$2
+
+slashed="${project}/"
+slashed_len=${#slashed}
+
+cicd=${tag:0:${slashed_len}}
+release=${tag:${slashed_len}}
+
+if [ "${cicd}" == "${slashed}" ]; then
+    if [ "${release}" == "" ]; then
+        echo "Warning! No release specified! Ignoring."
+        exit 2
+    fi
+else
+    echo "Warning! This can't be deployed, there's no ${project} tag! Is the travis condition set?"
+    exit 1
+fi
+
+file="${HOME}/.dockercfg"
+docker_repo="snowplow-docker-registry.bintray.io"
+curl -X GET \
+    -u${BINTRAY_SNOWPLOW_DOCKER_USER}:${BINTRAY_SNOWPLOW_DOCKER_API_KEY} \
+    https://${docker_repo}/v2/auth > $file
+
+cd $project
+# except for the base image, there is one folder per image version
+if [ "${project}" != "base" ]; then
+  cd $release
+fi
+
+img_tag="snowplow/${project}:${release}"
+final_tag="${docker_repo}/${img_tag}"
+docker build -t $final_tag .
+docker push $final_tag
