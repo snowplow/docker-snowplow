9a89f55ea8aefe36dbc31b0f1a0b51a435175f2c
Oguzhan Unlu <cengoguzhanunlu@gmail.com>
2018-07-27 20:52:07 +0300

    Bump to rc2

---
+++ iglu-server/0.3.1-rc2/docker-entrypoint.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/dumb-init /bin/sh
+set -e
+
+# If the config directory has been mounted through -v, we chown it.
+if [ "$(stat -c %u ${SNOWPLOW_CONFIG_PATH})" != "$(id -u snowplow)" ]; then
+  chown -R snowplow:snowplow ${SNOWPLOW_CONFIG_PATH}
+fi
+
+# Make sure we run the server as the snowplow user
+exec su-exec snowplow:snowplow /usr/bin/java \
+  $SP_JAVA_OPTS -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap \
+  -jar ${SNOWPLOW_BIN_PATH}/iglu-server-0.3.1-rc2.jar "$@"
