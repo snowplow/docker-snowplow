6936c2a466694c4cef438348d277a78ed8c4099b
Piotr Limanowski <piotr@codearsonist.com>
2020-01-07 16:42:25 +0100

    add image for k8s-based dataflow deployment
    Some dataflow jobs need to be deployed via k8s as there are significant limitations
    on how Scio behaves on Dataflow (mostly optional parameter handling).
    The kind of deployment has been a problem before. As the terraform resources are
    created asynchronously the jobs would fail for missing temporary buckets that
    are required to start. Therefore we need some sort of backoff strategy that will
    delay the deployment for a couple of seconds before the actual deployment trigger happens.

---
+++ k8s-dataflow/0.1.0/docker-entrypoint.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+DELAY=3
+THRESHOLD=5
+BUCKET=""
+
+ITER=0
+PARAMS=""
+
+# extract params
+for opt in "$@"; do
+  case "$opt" in
+      --tempLocation=*)
+          BUCKET="${opt#*=}"
+          PARAMS="$PARAMS $opt"
+          shift
+          ;;
+      --gcsThreshold=*)
+          THRESHOLD="${opt#*=}"
+          shift
+          ;;
+      --gcsDelay*)
+          DELAY="${opt#*=}"
+          shift
+          ;;
+      *) # preserve positional arguments
+          PARAMS="$PARAMS $opt"
+          shift
+          ;;
+  esac
+done
+
+if [ -z "$BUCKET" ]; then
+    echo "Missing --tempLocation flag. Exiting."
+    exit 1
+fi
+
+echo "params: ${PARAMS} threshold: ${THRESHOLD} delay: ${DELAY}"
+
+# enable service account
+gcloud auth activate-service-account --key-file=/snowplow/config/credentials.json
+
+# wait for GCS bucket to be available
+while [ ! -z $(gsutil ls -b "${BUCKET}" | grep BucketNotFoundException) ]; do
+    if [ "$ITER" -le "$THRESHOLD" ]; then
+        echo "Bucket ${BUCKET} does not exist, retrying: ${ITER}"
+        sleep "${DELAY}"
+
+        ITER=$(( ITER+1 ))
+    else
+        echo "Bucket ${BUCKET} does not exist. Not retrying anymore."
+        exit 1
+    fi
+done
+
+echo "Bucket ${BUCKET} exists. Proceeding."
+$PARAMS
